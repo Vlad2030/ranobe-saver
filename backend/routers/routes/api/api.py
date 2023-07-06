1aa50f3605ef8fc9562d1628e966c899e7446d42
vladdd0 <vlad123123v@gmail.com>
2023-07-06 13:47:27 +0300

    Change tags

--- backend/routers/routes/api/api.py
+++ backend/routers/routes/api/api.py
@@ -5,7 +5,7 @@ from routers.routes.api.v1 import v1
 
 def create_api_router() -> APIRouter:
     api_router = APIRouter()
-    api_router.include_router(v1.router, prefix="/v1", tags=["api/v1"])
+    api_router.include_router(v1.router, prefix="/v1", tags=["v1"])
     return api_router
 
 
