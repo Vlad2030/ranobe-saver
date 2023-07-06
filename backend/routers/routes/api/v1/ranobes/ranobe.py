c0469f73924d7245c8f1440ae558948f9f53e399
vladdd0 <vlad123123v@gmail.com>
2023-07-06 13:47:35 +0300

    Add test endpoint

--- backend/routers/routes/api/v1/ranobes/ranobe.py
+++ backend/routers/routes/api/v1/ranobes/ranobe.py
@@ -1,3 +1,12 @@
 from fastapi import APIRouter
 
 router = APIRouter(tags=["ranobes"])
+
+@router.get(
+    path="/",
+    status_code=200,
+    summary="Get ranobe",
+    description="Get ranobe from mangalib.me (´♡‿♡`)",
+)
+async def get_ranobe():
+    return {"ok": True}
