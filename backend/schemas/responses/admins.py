08de019f48e63dbc18d0fb65a1832bdd17720222
vladdd0 <vlad123123v@gmail.com>
2023-07-06 03:06:58 +0300

    Add enable orm_mode

--- backend/schemas/responses/admins.py
+++ backend/schemas/responses/admins.py
@@ -8,22 +8,37 @@ class Admin(BaseModel):
     full_name: str = "Admin"
     created_at: datetime
 
+    class Config:
+        orm_mode = True
+
 
 class AdminCreated(BaseModel):
     admin: Admin
     created: bool = True
+
+    class Config:
+        orm_mode = True
 
 
 class AdminDeleted(BaseModel):
     admin: Admin
     deleted: bool = True
 
+    class Config:
+        orm_mode = True
+
 
 class AdminById(BaseModel):
     admin: Admin
     is_exist: bool = True
 
+    class Config:
+        orm_mode = True
+
 
 class Admins(BaseModel):
     count: int
     admins: list[Admin]
+
+    class Config:
+        orm_mode = True
--- backend/schemas/responses/users.py
+++ backend/schemas/responses/users.py
@@ -8,22 +8,37 @@ class User(BaseModel):
     full_name: str = "User"
     created_at: datetime
 
+    class Config:
+        orm_mode = True
+
 
 class UserCreated(BaseModel):
     admin: User
     created: bool = True
+
+    class Config:
+        orm_mode = True
 
 
 class UserDeleted(BaseModel):
     admin: User
     deleted: bool = True
 
+    class Config:
+        orm_mode = True
+
 
 class UserById(BaseModel):
     admin: User
     is_exist: bool = True
 
+    class Config:
+        orm_mode = True
+
 
 class Users(BaseModel):
     count: int
     admins: list[User]
+
+    class Config:
+        orm_mode = True
